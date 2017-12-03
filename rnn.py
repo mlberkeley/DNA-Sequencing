a0c27f66a3c8367125af2f6a58f7dcb817c8af39
Daniel Ho <daniel.ho@berkeley.edu>
2017-12-02 17:11:55 -0800

    finished rnn, just predicting all 0s rn though

--- rnn.py
+++ rnn.py
@@ -42,12 +42,10 @@ seq_arrays = valid_seq #[np.array([i for i in s]) for s in valid_seq]
 
 valid_labels = ['Introduced', 'Invasive', 'Indigenous']
 labeled_cls = [label in valid_labels for label in cls_valid]
-#labeled_cls = (valid_labels[labeled_cls] == 'Indigenous').astype(int)
 
 # Create a filter telling us which points are valid to use for supervised training
 labeled_cls = np.array(labeled_cls)
 
-#print(len(labeled_cls))
 cls_valid[labeled_cls]
 
 # apply the filter over our features and labels
@@ -56,7 +54,21 @@ supervised_y = cls_valid[labeled_cls]
 
 supervised_y = (supervised_y == 'Indigenous').astype(int)
 
-supervised_X, test_X, supervised_y, test_y = sklearn.model_selection.train_test_split(supervised_X, supervised_y)
+# print('pre removal', len(supervised_X))
+
+# for i in range(1400, 0, -1):
+#     #print(i)
+#     if supervised_y[i] == 0:
+#         supervised_X = np.delete(supervised_X, i)
+#         supervised_y = np.delete(supervised_y, i)
+
+# print('post removal', len(supervised_X))
+
+
+supervised_X, test_X, supervised_y, test_y = sklearn.model_selection.train_test_split(supervised_X, supervised_y, test_size=320)
+
+print('train set:', len(supervised_X), 'test set:', len(test_X))
+
 
 n_categories = 2 # len(all_categories)
 n_letters = 5
@@ -105,21 +117,16 @@ class RNN(nn.Module):
 
         self.h0 = Variable(torch.zeros(2, 16, self.hidden_size).cuda())
         self.c0 = Variable(torch.zeros(2, 16, self.hidden_size).cuda())
-
-        self.test_h0 = Variable(torch.zeros(2, 421, self.hidden_size).cuda())
-        self.test_c0 = Variable(torch.zeros(2, 421, self.hidden_size).cuda())
     
     def forward(self, input, testing=False):
-        # combined = torch.cat((input, hidden), 1)
-        # h0 = Variable(torch.zeros(2, 16, self.hidden_size))
-        # c0 = Variable(torch.zeros(2, 16, self.hidden_size))
+        # if not testing:
+        h0, c0 = self.h0, self.c0
+        # else:
+        #     h0, c0 = self.test_h0, self.test_c0
+        # h0 = Variable(torch.zeros(2, input.size(1), self.hidden_size)).cuda()
+        # c0 = Variable(torch.zeros(2, input.size(1), self.hidden_size)).cuda()
 
-        print(type(input.data), input.size())
-
-        if testing:
-            output, hn = self.lstm(input, (self.test_h0, self.test_c0))
-        else:
-            output, hn = self.lstm(input, (self.h0, self.c0))
+        output, hn = self.lstm(input, (h0, c0))
 
         output = output[-1,:,:]
 
@@ -136,9 +143,12 @@ rnn = RNN(n_letters, n_hidden, n_categories)
 rnn.cuda()
 
 def categoryFromOutput(output):
+
     top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
-    category_i = top_i[0][0]
-    return bool(category_i), category_i
+
+    category_i = top_i[0]
+
+    return bool(category_i), int(category_i)
 
 
 def randomChoice(l):
@@ -154,7 +164,15 @@ def randomTrainingExample(num, X, y):
     line = X[rng]
     category_tensor = Variable(torch.LongTensor(category)).cuda()
     line_tensor = Variable(linesToTensor(line)).cuda()
-    # print(category_tensor.size(), line_tensor.size())
+    return category, line, category_tensor, line_tensor
+
+def randomTestingExample(i, num, X, y):
+    start = i*16
+    end = start + num
+    category = y[start:end]
+    line = X[start:end]
+    category_tensor = Variable(torch.LongTensor(category)).cuda()
+    line_tensor = Variable(linesToTensor(line)).cuda()
     return category, line, category_tensor, line_tensor
 
 
@@ -162,43 +180,63 @@ criterion = nn.NLLLoss()
 
 learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
 
-def train(category_tensor, line_tensor):
-    # hidden = rnn.initHidden()
+optim = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
 
-    rnn.zero_grad()
+train_accs = []
+train_losses = []
 
-    # print(line_tensor.size())
+def train(iter, category_tensor, line_tensor):
+    global train_accs, train_losses
 
+    optim.zero_grad()
     output = rnn(line_tensor)
-
     loss = criterion(output, category_tensor)
     loss.backward()
+    # optim.step()
 
-    # Add parameters' gradients to their values, multiplied by learning rate
     for p in rnn.parameters():
         p.data.add_(-learning_rate, p.grad.data)
 
-    return output, loss.data[0]
+    if iter % print_every > (print_every - 5):
+        loss_val = loss.data[0]
+        correct = 0
+        for gt, pred in zip(category_tensor.data, output):
+            if categoryFromOutput(pred)[1] == gt:
+                correct += 1
+
+        train_acc = correct / len(output)
+        train_accs.append(train_acc)
+        train_losses.append(loss_val)
+
+    if iter % print_every == 0:
+        print('training acc', np.mean(train_accs))
+        print('training loss:', np.mean(train_losses), 'iter:', iter)
+
+        train_accs = []
+        train_losses = []
+
+    return
 
 def test(category_tensor, line_tensor):
     rnn.zero_grad()
-
     output = rnn(line_tensor, testing=True)
 
-    total = 0
     correct = 0
+    zeros = 0
 
-    for gt, pred in zip(category_tensor, output):
-        total += 1
-        correct += gt == categoryFromOutput(pred)[1]
+    for gt, pred in zip(category_tensor.data, output):
+        pred_class = categoryFromOutput(pred)[1]
+        if pred_class == gt:
+            correct += 1
+        if pred_class == 0:
+            zeros += 1
 
-    acc = correct / float(total)
+    return correct, zeros
 
-    return acc
-
-n_iters = 100
-print_every = 1
-plot_every = 5
+n_iters = 10000
+print_every = 5
+plot_every = 50
+test_every = 10
 
 
 # Keep track of losses for plotting
@@ -214,31 +252,30 @@ def timeSince(since):
 
 start = time.time()
 
-for iter in range(1, n_iters + 1):
+test_batches = len(test_X)//16
+
+for iter in range(1, n_iters+1):
     category, line, category_tensor, line_tensor = randomTrainingExample(16, supervised_X, supervised_y)
-    # print(category_tensor.size(), line_tensor.size())
-    output, loss = train(category_tensor, line_tensor)
-    current_loss += loss
 
-    # # Print iter number, loss, name and guess
-    # if iter % print_every == 0:
-    #     guess, guess_i = categoryFromOutput(output)
-    #     correct = '✓' if guess == category else '✗ (%s)' % category
-    #     print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+    train(iter, category_tensor, line_tensor)
 
-    # Add current loss avg to list of losses
-    if iter % plot_every == 0:
-        all_losses.append(current_loss / plot_every)
-        current_loss = 0
-        print('testing', loss, iter)
+    # # Add current loss avg to list of losses
+    # if iter % plot_every == 0:
+    #     all_losses.append(current_loss / plot_every)
+    #     current_loss = 0
 
-    if iter % print_every == 0:
-        category, line, category_tensor, line_tensor = randomTrainingExample(len(test_X), test_X, test_y)
-        acc = test(category_tensor, line_tensor)
-        print('testing:', acc)
+    if iter % test_every == 0:
+        accs = []
+        total_zeros = 0
+        for i in range(test_batches):
+            category, line, category_tensor, line_tensor = randomTestingExample(i, 16, test_X, test_y)
+            acc, zeros = test(category_tensor, line_tensor)
+            accs.append(acc)
+            total_zeros += zeros
+        print('testing acc:', sum(accs)/len(test_X), 'total_zeros:', total_zeros, 'total values:', len(test_X))
 
 
-plt.figure()
-plt.plot(all_losses)
+# plt.figure()
+# plt.plot(all_losses)
 
 
